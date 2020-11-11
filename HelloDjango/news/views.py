8851eb5750914d4b29d4d735fd8184c2b7f0c54e
Eldar <elfarych@gmail.com>
2020-11-12 03:41:01 +0600

    Checkpoint

--- HelloDjango/news/views.py
+++ HelloDjango/news/views.py
@@ -121,7 +121,7 @@ def get_search_results(request):
 
 def post_detail(request, slug):
     post = Post.objects.get(slug=slug)
-    posts = Post.objects.defer('body').filter(public=True).exclude(slug=slug)[:4]
+    posts = Post.objects.defer('body').filter(public=True).exclude(slug=slug)[:8]
     post.views += 1
     post.save()
     description = get_text(post.body)[:170]
